0f79ddb2f091b7fae0299947811f23e919ed4545
Harshan_Nayak <128978924+Harshan-Nayak@users.noreply.github.com>
2025-01-07 01:38:31 +0530

    Time: 23 ms (73.74%) | Memory: 20.9 MB (78.10%) - LeetSync

---
+++ DFS/690-employee-importance/employee-importance.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    unordered_map<int, Employee*> employeeMap;
+
+    // DFS helper function
+    int dfs(int id) {
+        Employee* employee = employeeMap[id];
+        int totalImportance = employee->importance;
+
+        for (int subId : employee->subordinates) {
+            totalImportance += dfs(subId); // Recursive call for subordinates
+        }
+
+        return totalImportance;
+    }
+
+    int getImportance(vector<Employee*> employees, int id) {
+        // Map employees by ID for quick access
+        for (Employee* emp : employees) {
+            employeeMap[emp->id] = emp;
+        }
+
+        return dfs(id); // Start DFS from the given employee ID
+    }
+};
