cd9984615d8790f54c0749250db36f4e0f54ab12
Harshan_Nayak <128978924+Harshan-Nayak@users.noreply.github.com>
2025-01-14 11:54:25 +0530

    Time: 6 ms (52.56%) | Memory: 10 MB (47.48%) - LeetSync

---
+++ DFS/686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        int aLen = a.size();
+        int bLen = b.size();
+        int repetitions = bLen / aLen; // Minimum repetitions needed to cover 'b'
+
+        string repeatedA = a;
+        for (int i = 1; i <= repetitions + 2; ++i) {
+            if (repeatedA.find(b) != string::npos) {
+                return i;
+            }
+            repeatedA += a; // Add one more 'a' if not found
+        }
+        return -1; // If 'b' is not a substring even after sufficient repetitions
+    }
+};
