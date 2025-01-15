1ba1b8a976c5c493ac983554a9062eb4f7f155c1
Harshan_Nayak <128978924+Harshan-Nayak@users.noreply.github.com>
2025-01-15 14:58:27 +0530

    Time: 2 ms (81.13%) | Memory: 15.6 MB (45.60%) - LeetSync

--- DFS/22-generate-parentheses/generate-parentheses.cpp
+++ DFS/22-generate-parentheses/generate-parentheses.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    void backtrack(int open, int close, int n, string s, vector<string>& res) {
-        // Base case: If the current string has 2 * n characters, it's valid
-        if (s.length() == 2 * n) {
-            res.push_back(s); // Add the valid combination to the result
-            return;
-        }
-
-        // Add an open parenthesis if the number of open parentheses is less than n
-        if (open < n) {
-            backtrack(open + 1, close, n, s + "(", res);
-        }
-
-        // Add a close parenthesis if the number of close parentheses is less than open
-        if (close < open) {
-            backtrack(open, close + 1, n, s + ")", res);
-        }
+void backtrack(int open,int close, vector<string>&res,string s,int n){
+    if(s.length()==2*n){
+        res.push_back(s);
+        return ;
+    }
+    if(open<n){
+        backtrack(open+1,close,res,s + "(" ,n);
     }
 
+    if(close<open){
+        backtrack(open,close+1,res,s+")",n);
+    }
+
+
+
+
+
+
+}
     vector<string> generateParenthesis(int n) {
-        vector<string> res; // Result vector to store all valid combinations
-        string s = "";      // Current string being formed
-        backtrack(0, 0, n, s, res); // Start backtracking with 0 open and 0 close parentheses
+        vector<string>res;
+        string s ="";
+        backtrack(0,0,res,s,n);
+
+
+
+
         return res;
+        
     }
 };
