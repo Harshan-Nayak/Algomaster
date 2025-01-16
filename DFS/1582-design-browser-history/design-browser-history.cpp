3b831a482b4e077a6ed912f3285e329826324aca
Harshan_Nayak <128978924+Harshan-Nayak@users.noreply.github.com>
2025-01-16 23:06:14 +0530

    Time: 22 ms (74.85%) | Memory: 62.6 MB (88.54%) - LeetSync

--- DFS/1582-design-browser-history/design-browser-history.cpp
+++ DFS/1582-design-browser-history/design-browser-history.cpp
@@ -1,42 +1,37 @@
 class BrowserHistory {
 public:
-int pos = 0;
-vector<string>vec;
-    BrowserHistory(string homepage) {
-        vec.push_back(homepage);
-    }
-    
+    int pos = 0;
+    vector<string> vec;
+    BrowserHistory(string homepage) { vec.push_back(homepage); }
+
     void visit(string url) {
         int size = vec.size();
-        int n = size-pos;
+        int n = size - pos;
         n -= 1;
-        while(n--){
+        while (n--) {
             vec.pop_back();
         }
         vec.push_back(url);
         pos++;
-        
     }
-    
+
     string back(int steps) {
-        if(pos<steps){
-            steps=pos;
+        if (pos < steps) {
+            steps = pos;
         }
         pos -= steps;
         return vec[pos];
-        
     }
-    
+
     string forward(int steps) {
         int size = vec.size();
-        if(pos+steps>=size){
-            pos = size-1;
-        }else{
+        if (pos + steps >= size) {
+            pos = size - 1;
+        } else {
 
-        pos += steps;
+            pos += steps;
         }
-        return vec[pos]; 
-        
+        return vec[pos];
     }
 };
 
