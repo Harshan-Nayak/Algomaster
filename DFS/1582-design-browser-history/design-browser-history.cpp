5968707e5970efda841b9509c6f5b9f37e4aaaa0
Harshan_Nayak <128978924+Harshan-Nayak@users.noreply.github.com>
2025-01-16 23:02:08 +0530

    Time: 29 ms (52.96%) | Memory: 62.6 MB (88.54%) - LeetSync

---
+++ DFS/1582-design-browser-history/design-browser-history.cpp
@@ -0,0 +1,49 @@
+class BrowserHistory {
+public:
+int pos = 0;
+vector<string>vec;
+    BrowserHistory(string homepage) {
+        vec.push_back(homepage);
+    }
+    
+    void visit(string url) {
+        int size = vec.size();
+        int n = size-pos;
+        n -= 1;
+        while(n--){
+            vec.pop_back();
+        }
+        vec.push_back(url);
+        pos++;
+        
+    }
+    
+    string back(int steps) {
+        if(pos<steps){
+            steps=pos;
+        }
+        pos -= steps;
+        return vec[pos];
+        
+    }
+    
+    string forward(int steps) {
+        int size = vec.size();
+        if(pos+steps>=size){
+            pos = size-1;
+        }else{
+
+        pos += steps;
+        }
+        return vec[pos]; 
+        
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
