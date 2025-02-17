03a5bc9dc77182aae2317927fa2918f2e92369d9
Harshan_Nayak <128978924+Harshan-Nayak@users.noreply.github.com>
2025-02-17 14:58:02 +0530

    Time: 2256 ms (5.16%) | Memory: 333.1 MB (15.20%) - LeetSync

---
+++ DFS/808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -0,0 +1,23 @@
+
+
+class Solution {
+public:
+bool isSubsequence(string s, string t) {
+    auto it = t.begin();
+    for (char c : s) {
+        it = find(it, t.end(), c);
+        if (it == t.end()) return false;
+        ++it;
+    }
+    return true;
+}
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int count = 0;
+        for (string& word : words) {
+            if (isSubsequence(word, s)) {
+                count++;
+            }
+        }
+        return count;
+  }
+};
